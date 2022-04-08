09239fdf96d269ef70400553aaefb5ba6b6d8e8a
Ivan yuan <Ivan_Yuan@cmoney.com.tw>
2022-04-08 18:27:40 +0800

    環境建置

---
+++ src/components/Product.vue
@@ -0,0 +1,28 @@
+<template>
+	<main>
+		<section class="product"></section>
+		<ProductItem />
+	</main>
+</template>
+<script>
+import ProductItem from './ProductItem.vue';
+	export default {
+	components: { ProductItem },
+		name: "Product",
+		data() {
+			return {
+				data: [],
+			};
+		},
+		async created() {
+			try {
+				const api = "./data.json";
+				const res = await this.$http.get(api);
+				this.data = res.data;
+        console.log(this.data);
+			} catch (e) {
+        console.log('資料連結失敗:\n',e);
+      }
+		},
+	};
+</script>
